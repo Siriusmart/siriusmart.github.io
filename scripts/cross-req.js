e9265df971a94e3092e5c394110f7d40329d53cf
Siriusmart <71584876+Siriusmart@users.noreply.github.com>
2022-08-15 21:13:44 +0100

    hid iframes for cross site requests

--- scripts/cross-req.js
+++ scripts/cross-req.js
@@ -1,6 +1,6 @@
 function createIframe(url) {
 	let iframe = document.createElement('iframe');
-	// iframe.style.display = 'none';
+	iframe.style.display = 'none';
 	document.body.appendChild(iframe);
 	iframe.contentWindow.location.href = url;
 	iframe.contentWindow.close();
